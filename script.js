f26e4ef04f46a6ccb0ba1e8674b2e05fd876e366
Michael Kobela <mkobela@gmail.com>
2024-03-23 12:54:04 -0400

    Updated creating elements

--- script.js
+++ script.js
@@ -2,34 +2,42 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 
-console.log(itemInput.type);
-console.dir(itemInput);
-
 function addItem(e) {
-  e.preventDefault();
+  // e.preventDefault();
 
   const newItem = itemInput.value;
 
-  if (newItem.value === '') {
+  // Validate Input
+  if (newItem === '') {
     alert('Please add an item');
     return;
   }
 
-  const liElement = document.createElement('li');
-  const textNode = document.createTextNode(newItem);
-  liElement.appendChild(textNode);
+  // Create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
 
-  const button = document.createElement('button');
-  button.className = 'remove-item btn-link text-red';
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
 
-  const icon = document.createElement('icon');
-  icon.className = 'fa-solid fa-xmark';
+  itemList.appendChild(li);
 
-  button.appendChild(icon);
-  liElement.appendChild(button);
-
-  itemList.appendChild(liElement);
+  itemInput.value = '';
 }
 
-// Event listeners
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+// Event Listeners
 itemForm.addEventListener('submit', addItem);
